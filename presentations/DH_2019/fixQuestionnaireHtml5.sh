1da728cdaabe779724afbc8836bee6ff37854ce2
martindholmes <mholmes@uvic.ca>
2018-11-23 15:22:46 -0800

    About three quarters of the way through first draft of questionnaire.

--- presentations/DH_2019/fixQuestionnaireHtml5.sh
+++ presentations/DH_2019/fixQuestionnaireHtml5.sh
@@ -6,3 +6,6 @@ sed -i 's/schema.DC/schema.dc/g' questionnaire.htm
 sed -i 's/name="DC:/name="dc:/g' questionnaire.htm
 sed -i 's/dc:([A-Z])/dc:\L&/g' questionnaire.htm
 sed -i 's/&nbsp;/ /g' questionnaire.htm
+
+scp questionnaire.htm hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/endings/
+
