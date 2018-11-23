401badf4879ea4efaca3e36534352c4b41d34287
martindholmes <mholmes@uvic.ca>
2018-11-22 16:26:32 -0800

    Tweaks to source and output for HTML5 compliance.

---
+++ presentations/DH_2019/fixQuestionnaireHtml5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i 's|></meta>|/>|g' questionnaire.htm
+
+sed -i 's/schema.DC/schema.dc/g' questionnaire.htm
+sed -i 's/name="DC:/name="dc:/g' questionnaire.htm
+sed -i 's/dc:([A-Z])/dc:\L&/g' questionnaire.htm
+sed -i 's/&nbsp;/ /g' questionnaire.htm
