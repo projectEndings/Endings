ca933e720c56c1a519adefe9d563708f12a8a8c8
martindholmes <mholmes@uvic.ca>
2018-11-27 11:36:37 -0800

    Results page drafted; decoration added.

--- presentations/DH_2019/fixQuestionnaireHtml5.sh
+++ presentations/DH_2019/fixQuestionnaireHtml5.sh
@@ -6,4 +6,4 @@ sed -i 's/&nbsp;/ /g' questionnaire.htm
 java -jar ../../../diagnostics/utilities/saxon9he.jar -s:questionnaire.htm -xsl:quandary_source/quandaryFixes.xsl -o:questionnaire_fixed.htm
 
 scp questionnaire_fixed.htm hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/endings/questionnaire.htm
-
+scp -r images hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/endings/
